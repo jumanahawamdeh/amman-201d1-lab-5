6bdc8526643ce495fc319f6ff01438e7a6e9c2a1
Your Name In Single Quotes.jumana <your email here in single quotes.mjumana28@yahoo.com>
2019-10-12 15:59:48 +0300

    new

--- app.js
+++ app.js
@@ -105,8 +105,3 @@ function multiplyAnyArray(testDynamicArray) { //eslint-disable-line
 testMultiplyAnyArray(testDynamicArray);
 // // // // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
 
-
-
-
-
-
